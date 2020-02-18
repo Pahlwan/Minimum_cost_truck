167629daaa05a54ba901d7907143dea3bc9925f4
Pahlwan <30994711+Pahlwan@users.noreply.github.com>
2020-02-18 04:51:39 -0800

    Initial truck class

---
+++ datetime_test.py
@@ -0,0 +1,6 @@
+from datetime import datetime,timedelta
+
+d1=datetime.today()
+d2=datetime(2020,5,5)
+diff=(d2-d1)
+print(diff.days)
--- max_banifit.py
+++ max_banifit.py
@@ -1,10 +1,7 @@
 import pandas as pd 
-from datetime import datetime
+from datetime import datetime,timedelta
 
 class Truck:
-
-    
-
     def __init__(self,name,rate,minimum_run,cycle):
         self.name=name
         self.rate=rate
@@ -12,14 +9,20 @@ class Truck:
         self.cycle=cycle
     
     def days_reamining_in_cycle(self):
-        pass
+        return (self.cycle[1]-datetime.today())
 
 
 truck_list=[]
 
-t1=Truck("l1",30,3000,[datetime.now,datetime(2010-5-6)])
+truck_list.append(Truck("l1",30,3000,[datetime(2020,4,7),datetime(2020,5,6)]))
+truck_list.append(Truck("l2",25,2400,[datetime(2020,4,10),datetime(2020,5,9)]))
+truck_list.append(Truck("l3",34,2700,[datetime(2020,4,10),datetime(2020,5,9)]))
+truck_list.append(Truck("l2",25,3400,[datetime(2020,4,26),datetime(2020,5,25)]))
+truck_list.append(Truck("l2",29,2800,[datetime(2020,4,1),datetime(2020,4,30)]))
 
-print(t1)
+
+# dif=t1.days_reamining_in_cycle().days
+print(truck_list)
 
     
 
