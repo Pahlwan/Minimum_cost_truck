dcfe979b8d245bffe700f0af2912a82e6b189eec
Pahlwan <30994711+Pahlwan@users.noreply.github.com>
2020-02-18 07:00:35 -0800

    First full commit
    loading and unloading status not counted yet

--- max_benefit.py
+++ max_benefit.py
@@ -10,18 +10,20 @@ class Truck:
         self.alread_ran=already_ran
     
     def days_reamining_in_cycle(self):
-        return (self.cycle[1]-datetime.today())
+        return (self.cycle[1]-datetime(2020,4,27))
 
 
 def min_cost(truck_list,distance):
-    pay_after_pick_list=[]
+    
+    pay_after_pick_list=[] 
     cost_func=[]
     min_cost_func=[]
     for t in truck_list:
+        pay=0
         if(t.alread_ran+distance>t.minmum_run):
-            pay=t.rate+(t.alread_ran+distance-t.minmum_run)*(t.rate*120/100)
+            pay=t.rate*t.minmum_run+(t.alread_ran+distance-t.minmum_run)*(t.rate*120/100)
         else:
-            pay=t.rate
+            pay=t.rate*t.minmum_run
         pay_after_pick_list.append(pay)
 
         #more to impliment
@@ -42,12 +44,13 @@ truck_list=[]
 truck_list.append(Truck("l1",30,3000,2500,[datetime(2020,4,7),datetime(2020,5,6)]))
 truck_list.append(Truck("l2",25,2400,2400,[datetime(2020,4,10),datetime(2020,5,9)]))
 truck_list.append(Truck("l3",34,2700,2300,[datetime(2020,4,10),datetime(2020,5,9)]))
-truck_list.append(Truck("l4",25,3400,1500,[datetime(2020,4,26),datetime(2020,5,25)]))
-truck_list.append(Truck("l5",29,2800,2800,[datetime(2020,4,1),datetime(2020,4,30)]))
+truck_list.append(Truck("l4",25,3400,2500,[datetime(2020,4,26),datetime(2020,5,25)]))
+truck_list.append(Truck("l5",29,2800,2000,[datetime(2020,4,1),datetime(2020,4,30)]))
 
 
-print(min_cost(truck_list,600))
+list1=min_cost(truck_list,600)
 
+print(truck_list[list1.index(min(min_cost(truck_list,600)))].name)
 
 # dif=t1.days_reamining_in_cycle().days
 
