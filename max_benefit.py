285435d6a95adca0edb294f21e7c55c8e4f3776b
Pahlwan <30994711+Pahlwan@users.noreply.github.com>
2020-02-20 23:33:18 -0800

    major virsion 1.1
    Completet now easy usable

--- max_benefit.py
+++ max_benefit.py
@@ -4,45 +4,27 @@ from datetime import datetime,timedelta
 status_options=["LOADIG","WAITING","UNLOADING"]
 
 class Truck:
-    def __init__(self,name,rate,minimum_run,already_ran,cycle,status="Waiting"):
+    def __init__(self,name,rate,minimum_run,already_ran,cycle,status="WAITING"):
         self.name=name
         self.rate=rate
         self.minmum_run=minimum_run
         self.cycle=cycle
         self.alread_ran=already_ran
-        self.status=status
+        self.status=status.upper()
+        
     
-    def days_reamining_in_cycle(self):
-        return (self.cycle[1]-datetime(2020,4,27))
+    def days_reamining_in_cycle(self,to_day=datetime.today()):
+        
+        return (self.cycle[1]-to_day)
 
 
 # This calculate eficiency for each truck
-def min_cost(truck_list,asssignment_distance,status_consideration=False):
+def min_cost(truck_list,asssignment_distance,to_day,status_consideration=False):
     
-    # pay_after_pick_list=[] 
-    # cost_func=[]
-    # min_cost_func=[]
-    # for t in truck_list:
-    #     pay=0
-    #     if(t.alread_ran+distance>t.minmum_run):
-    #         pay=t.rate*t.minmum_run+(t.alread_ran+distance-t.minmum_run)*(t.rate*120/100)
-    #     else:
-    #         pay=t.rate*t.minmum_run
-    #     pay_after_pick_list.append(pay)
-
-    #     #more to impliment
-    #     if(t.minmum_run-t.alread_ran==0):
-    #         cost_func.append(t.days_reamining_in_cycle().days/1)
-    #     else:
-    #         cost_func.append(t.days_reamining_in_cycle().days/(t.minmum_run-t.alread_ran))
-
-    # for i in range(len(truck_list)):
-    #     min_cost_func.append(cost_func[i]*pay_after_pick_list[i])
-
-    # return min_cost_func
+    
     if status_consideration==True:
         for t in truck_list:
-            if t.status=="Unloading":
+            if t.status=="Unloading".upper():
                 truck_list.remove(t)
     
     cost_efficiency=[]
@@ -51,14 +33,14 @@ def min_cost(truck_list,asssignment_distance,status_consideration=False):
         total_run_after_assignment=i.alread_ran+asssignment_distance
 
         if total_run_after_assignment>i.minmum_run and (i.minmum_run-i.alread_ran)!=0:
-            cost_efficiency.append(((i.rate*i.minmum_run)+(i.rate*120/100)*(total_run_after_assignment-i.minmum_run))*((i.minmum_run-i.alread_ran)/i.minmum_run)*i.days_reamining_in_cycle().days)
+            cost_efficiency.append(((i.rate*i.minmum_run)+(i.rate*120/100)*(total_run_after_assignment-i.minmum_run))*((i.minmum_run-i.alread_ran)/i.minmum_run)*i.days_reamining_in_cycle(to_day).days)
 
         elif (i.minmum_run-i.alread_ran)!=0 and total_run_after_assignment<=i.minmum_run:
             # Main formula
-            cost_efficiency.append((i.rate*i.minmum_run)*((i.minmum_run-i.alread_ran)/i.minmum_run)*i.days_reamining_in_cycle().days)
+            cost_efficiency.append((i.rate*i.minmum_run)*((i.minmum_run-i.alread_ran)/i.minmum_run)*i.days_reamining_in_cycle(to_day).days)
 
         else:
-            cost_efficiency.append((i.rate*i.minmum_run)*(1)*i.days_reamining_in_cycle().days)
+            cost_efficiency.append((i.rate*i.minmum_run)*(1)*i.days_reamining_in_cycle(to_day).days)
 
     return cost_efficiency
 
@@ -84,10 +66,10 @@ def my_own_trucks():
             except:
                 print("Invailid date format input date again")
                 continue
-
             else:
                 break
-        status=input().upper   
+            
+        status=input("Status of truck : ").upper()   
            
         tepm_t=Truck(name,rate,minimum_run,already_ran,cycle,status)
         truck_list.append(tepm_t)
@@ -103,14 +85,29 @@ truck_list.append(Truck("l3",34,2700,2300,[datetime(2020,4,10),datetime(2020,5,9
 truck_list.append(Truck("l4",25,2400,100,[datetime(2020,4,26),datetime(2020,5,25)]))
 truck_list.append(Truck("l5",30,2800,2000,[datetime(2020,4,1),datetime(2020,4,30)]))
 
+
+to_day=datetime.today()
+
 while(1):
+    print("Configure testing first".center(80,'-')+"\n")
+    if input("Want a custom date for today press Y/N : ").upper()=="Y":
+        to_day=datetime.strptime(input("Format YYYY/MM/DD : ").strip(),"%Y/%m/%d")
+    if input("If you want to consider loading status press Y/N : ").upper()=='Y':
+        loading_status_consideration=True
+    else:
+        loading_status_consideration=False
+    print("")
+    print(''.center(80,'-'))
+    
     print("1: Use sample date")
     print("2: Input your own data")
     print("3: For exit")
-    print("   \nPRESS 1 OR 2 OR 3")
+    print("   \nPRESS 1 OR 2 OR 3 : ",end="")
     i=int(input())
 
+    
     # Custom input
+    
     if i==2:
         truck_list=my_own_trucks()
         print("".center(80,"-"))
@@ -120,7 +117,7 @@ while(1):
         exit()   
         
         
-    list1=min_cost(truck_list,600,True)
+    list1=min_cost(truck_list,600,to_day,loading_status_consideration)
 
     # Printing efficiency of each truck
     print("Cost efficiency for each truck :")
@@ -135,7 +132,7 @@ while(1):
     print(truck_list[list1.index(min(list1))].name)
 
     print()
-    if(input("Test more Y/N").upper()=="N"):
+    if(input("Test more Y/N : ").upper()=="N"):
         print("Thank you for using me")
         exit()
 
